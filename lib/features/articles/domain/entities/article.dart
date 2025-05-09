43fa7c0bd4c5e3ddf1e9a95b58f2fe79b8dbc44e
“Kaushal <kaushaljhod@gmail.com>
2025-05-10 03:30:19 +0530

    chore: Article Entity added for articles feature

---
+++ lib/features/articles/domain/entities/article.dart
@@ -0,0 +1,13 @@
+class Article {
+  final int id;
+  final int userId;
+  final String body;
+  final String title;
+
+  Article({
+    required this.id,
+    required this.userId,
+    required this.body,
+    required this.title,
+  });
+}
