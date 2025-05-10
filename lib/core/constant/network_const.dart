4cdae45ce5d330feb9e60b9d68b38bee2ae2775f
“Kaushal <kaushaljhod@gmail.com>
2025-05-11 02:39:29 +0530

    chore(core): Added NetworkConst for baseURL and network timeouts

---
+++ lib/core/constant/network_const.dart
@@ -0,0 +1,5 @@
+class NetworkConst {
+  static const String baseURL = "https://jsonplaceholder.typicode.com";
+  static const Duration connectionTimeout = Duration(seconds: 12);
+  static const Duration recieveTimeout = Duration(seconds: 12);
+}
