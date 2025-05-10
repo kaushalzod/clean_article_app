b56ad2d57fab3da9ac01ab665eab9b4c8e9470f0
“Kaushal <kaushaljhod@gmail.com>
2025-05-10 18:50:01 +0530

    chore(domain): Abstract ArticleRepository created for articles related functions

---
+++ lib/features/articles/domain/repositories/article_repository.dart
@@ -0,0 +1,23 @@
+import 'package:article_app/core/model/either.dart';
+import 'package:article_app/core/model/failure.dart';
+import 'package:article_app/features/articles/domain/entities/article.dart';
+
+abstract class ArticleRepository {
+  /// Returns List of all the articles from remote data source
+  Future<Either<Failure, List<Article>>> getArticles();
+
+  /// Return specific article by its id
+  Future<Either<Failure, Article>> getArticleById(int id);
+
+  /// Search Articles by title or body
+  Future<Either<Failure, List<Article>>> searchArticles(String query);
+
+  /// Get List of all the Favourite Articles
+  Future<Either<Failure, List<Article>>> getFavouriteArticles();
+
+  /// Toggle favourite article in local data source
+  Future<Either<Failure, Article>> toggleFavouriteArticle();
+
+  /// Returns bool if given article id is marked as favourite
+  Future<Either<Failure, bool>> isFavouriteArticle(int id);
+}
